cbb275d351cbe5c9147358b5df25e95ac962dc40
udaysaigorijavolu <udaysai4701@gmail.com>
2022-10-06 12:27:18 +0530

    intial commit

---
+++ Swap-Shop/src/main/java/com/swapshop/SwapShopApplication.java
@@ -0,0 +1,13 @@
+package com.swapshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapShopApplication.class, args);
+	}
+
+}
---
+++ Swap-Shop/src/test/java/com/swapshop/SwapShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swapshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
